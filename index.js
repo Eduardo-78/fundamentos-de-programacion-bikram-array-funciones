ee53b982494379293a96cd6fb6dce334ef339d66
Eduardo-78 <145039634+Eduardo-78@users.noreply.github.com>
2023-09-27 00:13:13 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,150 @@
+//01
+let arrayVacio = [];
+
+//02
+let = arrayNumeros = [0,1,2,3,4,5,6,7,8,9];
+
+//03
+let arrayNumerosPares = [0,2,4,6,8];
+
+//04
+let arrrayBidimensional = [[0,1,2],['a','b','c']];
+
+//05
+function suma (a,b)
+{
+    return a+b;
+}
+
+//06
+function potenciacion (a,b)
+{
+    
+    return a**b;
+}
+
+//07
+function separarPalabras (str)
+{
+    return str.split('');
+}
+
+//08
+function repetirString (string, numero)
+{
+    let valor = "";
+    for (let i = 0; i < numero; i++)
+    {
+        valor = palabra + valor;
+    }
+}
+    
+//09
+function esPrimo (num)
+    {
+        if (num % i === 0)
+        {
+            return false;
+        }  
+    return true;
+  }
+
+//10
+function ordenarArray (arraynum)
+{
+    return arraynum.sort((a,b) => a-b);
+
+}
+
+//11
+function obtenerPares (arraynum)
+{
+    let lisPar = [];
+    for (let i = 0; i&lt;array.length; i++)
+    {
+        if (array[i] % 2 === 0)
+        {
+            lisPar.push(array[i]);
+        }
+    }
+    return lisPar;
+}
+
+//12
+function pintarArray (array)
+{
+    return `[${array}]`.split(',').join(',');
+}
+
+//13
+function arrayMapi(array, funcion)
+{
+    return array.map(funcion);
+}
+
+//14
+function eliminarDuplicados (array)
+{
+    return array.from(new setInterval(array));
+}
+
+//15
+let arrayNumerosNeg = [0,-1,-2,-3,-4,-5,-6,-7,-8,-9];
+
+//16
+let holaMundo = ['Hola', 'Mundo'];
+
+//17
+let loGuardoTodo = ['hola', 'que', 23, 42.33, 'tal'];
+
+//18
+let arrayDeArrays = [[756, 'nombre'], [225,'apellido'], [298, 'direccion']];
+
+//19
+function multiplicacion (a,b)
+{
+    return a*b;
+}
+
+//20
+function division (a,b) 
+{
+    return a/b;
+}
+
+//21
+function esPar (num)
+{
+    if (num % 2 === 0) 
+    {
+        return true;
+    } 
+    else 
+    {
+        return false;
+    }
+}
+
+//22
+const arrayFunciones = [function suma(a, b) { return a + b;},
+    function resta(a, b) { return a - b;},
+    function multiplicacion(a, b) { return a * b;}];
+
+//23
+function ordenarArray2 (array)
+{
+    return array.sort(function (a, b) {return b-a});
+   
+}
+
+//24
+function obtenerImpares (array)
+{    
+    if (array[i] % 2 !== 0)
+    {
+        impar.push(array[i]);
+    };    
+        return impar;
+}
+
+//25
